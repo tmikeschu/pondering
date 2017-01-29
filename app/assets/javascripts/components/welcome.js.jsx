a3e2ced2cdd4bca46966b8a7828588425b1344b3
Mike Schutte <tmikeschutte@gmail.com>
2017-01-28 23:55:43 -0700

    Extract welcome component

---
+++ app/assets/javascripts/components/welcome.js.jsx
@@ -0,0 +1,10 @@
+var Welcome = React.createClass({
+  render () {
+    return (
+      <section className="welcome">
+        <h1>Welcome to Pondering</h1>
+        <p>a place to get lost</p>
+      </section>
+    )
+  }
+});
