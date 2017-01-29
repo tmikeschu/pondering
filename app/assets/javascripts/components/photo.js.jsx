6b0181851577ef79d08f08f73465a60bae0169c7
Mike Schutte <tmikeschutte@gmail.com>
2017-01-28 23:56:03 -0700

    Add photo component

---
+++ app/assets/javascripts/components/photo.js.jsx
@@ -0,0 +1,9 @@
+var Photo = React.createClass({
+  render() {
+    return (
+      <section className="photo">
+        <img src="https://s-media-cache-ak0.pinimg.com/236x/02/17/fa/0217faa4735ad5a1550aa45d8f5da961.jpg" alt="magical doodle"/>
+      </section>
+    );
+  }
+});
